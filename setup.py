4c14b9698e6bcfc67208e27da110709f76ed4201
Eric Sandine <ericsandine@gmail.com>
2019-09-24 10:06:15 -0700

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
-version = '0.1'
+version = '0.2'
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
